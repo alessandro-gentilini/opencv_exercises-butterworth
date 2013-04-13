0ddf1d4fca66abd0b53f7a0b650b8e3e56979afa
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2013-04-13 16:58:39 +0200

    removed obsolete code

--- butterworth.cpp
+++ butterworth.cpp
@@ -322,136 +322,22 @@ int main( int argc, char **argv )
 
     return -1;
 }
-/******************************************************************************/
 
+double deg2rad( double a )
+{
+    return (M_PI*a)/180;
+}
 
-// #include "opencv2/core/core.hpp"
-// #include "opencv2/imgproc/imgproc.hpp"
-// #include "opencv2/highgui/highgui.hpp"
-// #include <iostream>
+void create_a_model()
+{
+    cv::Mat img(360, 360, cv::DataType<unsigned char>::type);
+    for ( size_t i = 0; i < img.cols; i+=1 )
+    {
+        for ( size_t j = 0; j < img.rows; j+=1 )
+        {
+            img.at<unsigned char>(i,j) = 127 + 127 * sin(deg2rad(20*j));
+        }
 
-// using namespace cv;
-
-// void create_a_model();
-
-// int main(int argc, char **argv)
-// {
-//     const char *filename = argc >= 2 ? argv[1] : "lena.jpg";
-
-//     // Load an image
-//     cv::Mat inputImage = cv::imread(filename, 0);
-
-//     imshow("initial",inputImage);
-
-//     // Go float
-//     cv::Mat fImage;
-//     inputImage.convertTo(fImage, CV_32F);
-
-//     // FFT
-//     std::cout << "Direct transform...\n";
-//     cv::Mat fourierTransform;
-//     cv::dft(fImage, fourierTransform, cv::DFT_SCALE | cv::DFT_COMPLEX_OUTPUT);
-
-//     std::cout << fourierTransform.channels() << "\n";
-//     cv::Mat planes[2];
-//     cv::split(fourierTransform,planes);
-//     cv::Mat mag;
-//     cv::magnitude(planes[0],planes[1],mag);
-//     cv::imshow("Mag",mag);
-//     std::cout << mag;
-
-//     cv::Mat pha;
-//     cv::phase(planes[0],planes[1],pha);
-//     cv::imshow("Pha",pha);
-
-//     // IFFT
-//     std::cout << "Inverse transform...\n";
-//     cv::Mat inverseTransform;
-//     cv::dft(fourierTransform, inverseTransform, cv::DFT_INVERSE | cv::DFT_REAL_OUTPUT);
-
-//     // Back to 8-bits
-//     cv::Mat finalImage;
-//     inverseTransform.convertTo(finalImage, CV_8U);
-
-//     imshow("final",finalImage);
-
-
-//     Mat I = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
-//     if ( I.empty())
-//         return -1;
-
-//     Mat padded;                            //expand input image to optimal size
-//     int m = getOptimalDFTSize( I.rows );
-//     int n = getOptimalDFTSize( I.cols ); // on the border add zero values
-//     copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, BORDER_CONSTANT, Scalar::all(0));
-
-//     Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
-//     Mat complexI;
-//     merge(planes, 2, complexI);         // Add to the expanded another plane with zeros
-
-//     dft(complexI, complexI);            // this way the result may fit in the source matrix
-
-//     // compute the magnitude and switch to logarithmic scale
-//     // => log(1 + sqrt(Re(DFT(I))^2 + Im(DFT(I))^2))
-//     split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
-//     magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
-//     Mat magI = planes[0];
-
-//     magI += Scalar::all(1);                    // switch to logarithmic scale
-//     log(magI, magI);
-
-//     // crop the spectrum, if it has an odd number of rows or columns
-//     magI = magI(Rect(0, 0, magI.cols & -2, magI.rows & -2));
-
-//     // rearrange the quadrants of Fourier image  so that the origin is at the image center
-//     int cx = magI.cols / 2;
-//     int cy = magI.rows / 2;
-
-//     Mat q0(magI, Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
-//     Mat q1(magI, Rect(cx, 0, cx, cy));  // Top-Right
-//     Mat q2(magI, Rect(0, cy, cx, cy));  // Bottom-Left
-//     Mat q3(magI, Rect(cx, cy, cx, cy)); // Bottom-Right
-
-//     Mat tmp;                           // swap quadrants (Top-Left with Bottom-Right)
-//     q0.copyTo(tmp);
-//     q3.copyTo(q0);
-//     tmp.copyTo(q3);
-
-//     q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
-//     q2.copyTo(q1);
-//     tmp.copyTo(q2);
-
-//     normalize(magI, magI, 0, 1, CV_MINMAX); // Transform the matrix with float values into a
-//     // viewable image form (float between values 0 and 1).
-
-//     imshow("Input Image"       , I   );    // Show the result
-//     imshow("spectrum magnitude", magI);
-
-//     imwrite("spectrum.bmp",magI);
-
-//     create_a_model();
-
-
-//     waitKey();
-
-//     return 0;
-// }
-
-// double deg2rad( double a )
-// {
-//     return (M_PI*a)/180;
-// }
-
-// void create_a_model()
-// {
-//     cv::Mat img(360, 360, cv::DataType<unsigned char>::type);
-//     for ( size_t i = 0; i < img.cols; i+=1 )
-//     {
-//         for ( size_t j = 0; j < img.rows; j+=1 )
-//         {
-//             img.at<unsigned char>(i,j) = 127 + 127 * sin(deg2rad(20*j));
-//         }
-
-//     }
-//     imwrite("sin.bmp", img);
-// }
+    }
+    imwrite("sin.bmp", img);
+}
